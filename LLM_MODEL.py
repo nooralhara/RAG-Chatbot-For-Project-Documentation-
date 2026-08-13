c2112bcbcd760b80d8d9b61adf210796f7f315ea
NoorAldeen AlHara <157886292+nooralhara@users.noreply.github.com>
2026-08-13 16:57:23 +0300

    Integrate Streamlit for API key management

--- LLM_MODEL.py
+++ LLM_MODEL.py
@@ -1,12 +1,13 @@
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 import os
+import streamlit as st
 
 
 load_dotenv()
 
 LLM = ChatOpenAI (
-    api_key = os.getenv("OPENROUTER_API_KEY"),
+    api_key = st.secrets("OPENROUTER_API_KEY"),
     base_url = "https://openrouter.ai/api/v1",
     model = "openrouter/free",
     temperature = 0.8
