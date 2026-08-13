cbcffb048d71125833979ffddfce41d719ba9b0b
NoorAldeen AlHara <157886292+nooralhara@users.noreply.github.com>
2026-08-13 17:05:08 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,29 +2,37 @@ from RAG import retriever
 from LLM_MODEL import LLM
 from prompts import prompt
 from langchain_core.messages import HumanMessage
+import streamlit as st
 
-print("App Started")
+st.title("ML Projects RAG Chatbot")
 
-while True:
-    question = input("\nYou")
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-    if question.lower() == "exit":
-        break
+for msg in st.session_state.messages:
+    with st.chat_message(msg["role"]):
+        st.write(msg["content"])
+
+question = st.chat_input("Ask about my ML projects...")
+
+if question:
+    st.session_state.messages.append({"role": "user", "content": question})
+    with st.chat_message("user"):
+        st.write(question)
 
     docs = retriever.invoke(question)
 
-    context = "\n\n".join(
-        doc.page_content for doc in docs
-    )
+    context = "\n\n".join(doc.page_content for doc in docs)
 
     final_prompt = prompt.format(
-        Context = context,
-        question = question
+        Context=context,
+        question=question
     )
 
     response = LLM.invoke([
-        HumanMessage(content = final_prompt)
+        HumanMessage(content=final_prompt)
     ])
 
-    print("\nAssistant: ", response.content)
-
+    st.session_state.messages.append({"role": "assistant", "content": response.content})
+    with st.chat_message("assistant"):
+        st.write(response.content)
